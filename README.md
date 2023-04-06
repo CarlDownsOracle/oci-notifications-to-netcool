3fac128e47e0a9471e57621614d0d301dde65453
Carl Downs <carl.downs@oracle.com>
2023-04-06 18:24:58 -0500

    initial check in

---
+++ README.md
@@ -0,0 +1,137 @@
+# Exporting OCI Notifications to Netcool
+
+---
+
+## Overview
+
+This sample OCI Function maps OCI Notifications to IBM® Tivoli® Netcool/OMNIbus Probe for Message Bus
+See https://www.ibm.com/docs/en/SSSHTQ_int/pdf/messbuspr-pdf.pdf
+
+
+### Prerequisites
+
+If you’re new to Functions, get familiar by running through 
+the [Quick Start guide on OCI Functions](http://docs.oracle.com/en-us/iaas/Content/Functions/Tasks/functionsquickstartguidestop.htm) before proceeding.
+
+---
+## Monitoring Service
+
+ The [Monitoring Service](https://docs.oracle.com/en-us/iaas/Content/Monitoring/Concepts/monitoringoverview.htm)
+ receives timestamp-value pairs (aka metric data points) which also carry contextual 
+dimensions and metadata about the services or applications that emitted them. 
+
+---
+## Functions Service
+
+I need to transform between the raw metrics formats and some way to make the API calls. The 
+[OCI Functions Service](http://docs.oracle.com/en-us/iaas/Content/Functions/Concepts/functionsoverview.htm) is a 
+natural fit for the task. Functions integrate nicely with Service Connector Hub as-as a target and can scale up
+depending on the demand.  That lets me focus on writing the logic needed without needing to address how to 
+deploy and scale it.
+
+---
+## Mapping From OCI to Netcool Formats
+
+A key requirement of course is the mapping of OCI to Netcool format.  Let's compare the OCI and Netcool
+message payload formats, what the mapping needs to accomplish, and see what the resulting transformed message 
+looks like.
+
+Example OCI Notifications Payload:
+    
+     {
+       "dedupeKey": "3d5c2f16-....",
+       "title": "ontario-health-alarm",
+       "body": "ontario-health test alarm",
+       "type": "OK_TO_FIRING",
+       "severity": "INFO",
+       "timestampEpochMillis": 1680821220000,
+       "timestamp": "2023-04-06T22:47:00Z",
+       "alarmMetaData": [
+         {
+           "id": "ocid1.alarm.oc1.phx.....",
+           "status": "FIRING",
+           "severity": "INFO",
+           "namespace": "oci_objectstorage",
+           "query": "AllRequests[1m]{resourceDisplayName = \"ontario-health-bucket\"}.count() > 1",
+           "totalMetricsFiring": 1,
+           "dimensions": [
+             {
+               "resourceID": "ocid1.bucket.oc1.phx.....",
+               "resourceDisplayName": "ontario-health-bucket"
+             }
+           ],
+           "alarmUrl": "https://cloud.oracle.com/monitoring/alarms/ocid1.alarm.oc1.phx..."
+         }
+       ],
+       "version": 1.3
+     }
+
+Example Netcool Payload:
+
+     {
+       "receiver": "Sandbox - Info",
+       "status": "firing",
+       "alerts": [
+         {
+           "status": "firing",
+           "labels": {
+             "alertname": "UpdateAvailable",
+             "channel": "eus-4.10",
+             "openshift_io_alert_source": "platform",
+             "prometheus": "openshift-monitoring/k8s",
+             "severity": "info",
+             "upstream": "\u003cdefault\u003e"
+           },
+           "annotations": {
+             "description": "For more information refer to ...",
+             "summary": "Your upstream update recommendation service recommends you update your cluster."
+           }
+         }
+       ]
+     }
+
+Mapping Behavior:
+
+     TBD
+
+Resulting Output:
+
+     TBD
+
+---
+## Policy Setup
+
+You will need to authorize the Notifications Service to invoke your Function.
+
+    Allow service notification to use fn-app in compartment {compartment name}
+
+---
+## Function Environment
+
+Here are the supported Function parameters:
+
+| Environment Variable | Default           | Purpose                                                                               |
+|----------------------|:-------------:|:--------------------------------------------------------------------------------------|
+| API_ENDPOINT         | not-configured | REST API endpoint                                                                     |
+| API_KEY              | not-configured      | API license token                                                                     |
+| LOGGING_LEVEL        | INFO     | Controls function logging outputs.  Choices: INFO, WARN, CRITICAL, ERROR, DEBUG       |
+| FORWARD_TO_ENDPOINT  | True      | Determines whether messages are forwarded to the API endpoint (useful during testing) |
+
+
+---
+## **OCI** Related Workshops
+
+LiveLabs is the place to explore Oracle's products and services using workshops designed to 
+enhance your experience building and deploying applications on the Cloud and On-Premises.
+ur library of workshops cover everything from how to provision the world's first autonomous 
+database to setting up a webserver on our world class OCI Generation 2 infrastructure, 
+machine learning and much more.  Use your existing Oracle Cloud account, 
+a [Free Tier](https://www.oracle.com/cloud/free/) account or a LiveLabs Cloud Account to build, test, 
+and deploy applications on Oracle's Cloud.
+
+Visit [LiveLabs](http://bit.ly/golivelabs) now to get started.  Workshops are added weekly, please visit frequently for new content.
+
+---
+## License
+Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
---
+++ func.py
@@ -0,0 +1,191 @@
+#
+# oci-monitoring-notifications-to-netcool version 1.0.
+#
+# Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+import io
+import json
+import logging
+import os
+import requests
+
+"""
+This sample OCI Function maps OCI Notifications to IBM® Tivoli® Netcool/OMNIbus Probe for Message Bus
+See https://www.ibm.com/docs/en/SSSHTQ_int/pdf/messbuspr-pdf.pdf
+"""
+
+
+# Use OCI Application or Function configurations to override these environment variable defaults.
+
+api_endpoint = os.getenv('API_ENDPOINT', 'not-configured')
+api_key = os.getenv('API_KEY', 'not-configured')
+is_forwarding = eval(os.getenv('FORWARD_TO_ENDPOINT', "True"))
+
+# Set all registered loggers to the configured log_level
+
+logging_level = os.getenv('LOGGING_LEVEL', 'INFO')
+loggers = [logging.getLogger()] + [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+[logger.setLevel(logging.getLevelName(logging_level)) for logger in loggers]
+
+
+def handler(ctx, data: io.BytesIO = None):
+    """
+    OCI Function Entry Point
+    :param ctx: InvokeContext
+    :param data: data payload
+    :return: plain text response indicating success or error
+    """
+
+    preamble = " {} / notification event count = {} / logging level = {} / forwarding to endpoint = {}"
+
+    try:
+        event_list = json.loads(data.getvalue())
+        logging.getLogger().info(preamble.format(ctx.FnName(), len(event_list), logging_level, is_forwarding))
+        logging.getLogger().debug(event_list)
+        send_payload(event_list=transform_payload(event_list=event_list), ctx=ctx)
+
+    except (Exception, ValueError) as ex:
+        logging.getLogger().error('error in handler: {}'.format(str(ex)))
+        logging.getLogger().error(ex)
+
+
+def transform_payload(event_list):
+    """
+    """
+
+    if isinstance(event_list, list) is False:
+        event_list = [event_list]
+
+    result_list = []
+    for event in event_list:
+        single_result = transform_event(event=event)
+        result_list.append(single_result)
+        logging.getLogger().debug(single_result)
+
+    return result_list
+
+
+def transform_event(event: dict):
+    """
+    This is performing no transformation at the moment
+    """
+
+    return event
+
+    # notifications = [{
+    #     'receiver': get_receiver_name(event),
+    #     'status': get_status(event),
+    #     'alerts': get_alerts(event),
+    #
+    # }]
+    #
+    # result = {
+    #     'notifications': notifications
+    # }
+    # return result
+
+
+def send_payload(event_list, ctx):
+    """
+    """
+
+    if is_forwarding is False:
+        logging.getLogger().debug("Endpoint forwarding is disabled - nothing sent")
+        return
+
+    # creating a session and adapter to avoid recreating
+    # a new connection pool between each POST call
+
+    session = requests.Session()
+
+    try:
+        adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10)
+        session.mount('https://', adapter)
+
+        api_headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        add_custom_headers(headers=api_headers, ctx=ctx)
+
+        for event in event_list:
+            logging.getLogger().debug("json to netcool: {}".format(json.dumps(event)))
+            response = session.post(api_endpoint, data=json.dumps(event), headers=api_headers)
+
+            if response.status_code != 200:
+                raise Exception ('error {} sending to Netcool: {}'.format(response.status_code, response.reason))
+
+    finally:
+        session.close()
+
+
+def add_custom_headers(headers, ctx):
+    """
+    header1 --- Authorization=Bearer ++Oauth.access_token++
+    header2 --- Accept=application/json
+    header3 --- Content-Type=application/json
+    """
+
+    if ctx is None:
+        return
+
+    fn_config = ctx.Config()
+    logging.debug('fn_config: {} ... {}'.format(type(fn_config), fn_config))
+
+    for x, y in fn_config.items():
+
+        if x.startswith('header'):
+            assignment_parts = y.split('=')
+
+            if len(assignment_parts) != 2:
+                continue
+
+            lvalue = assignment_parts[0]
+            rvalue = assignment_parts[1]
+            headers[lvalue] = rvalue
+
+
+def local_test_linebreak_events_file(filename):
+    """
+    """
+
+    logging.getLogger().info("local_test_linebreak_events_file testing started")
+
+    with open(filename, 'r') as f:
+        transformed_results = list()
+
+        for line in f:
+            event = json.loads(line)
+            logging.getLogger().debug(json.dumps(event, indent=4))
+            transformed_result = transform_event(event)
+            transformed_results.append(transformed_result)
+
+        logging.getLogger().debug(json.dumps(transformed_results, indent=4))
+        send_payload(event_list=transformed_results, ctx=None)
+
+    logging.getLogger().info("local testing completed")
+
+
+def local_test_single_event_file(filename):
+    """
+    """
+
+    logging.getLogger().info("local_test_single_event_in_file testing started")
+
+    with open(filename, 'r') as f:
+        transformed_results = list()
+        event = json.load(f)
+        transformed_result = transform_event(event)
+        transformed_results.append(transformed_result)
+
+        logging.getLogger().debug(json.dumps(transformed_results, indent=4))
+        send_payload(event_list=transformed_results, ctx=None)
+
+    logging.getLogger().info("local testing completed")
+
+
+"""
+Local Debugging 
+"""
+
+if __name__ == "__main__":
+    local_test_single_event_file('test/netcool.outbound.json')
+
